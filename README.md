256b709377c44b161d6bd7972ee838b058cd1292
Antonio Marino <marino.antonio375@gmail.com>
2021-12-15 12:00:59 -0500

    adding react template

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# React_Portfolio
-Re-Creating our portfolio with reactJS
+# React-Portfolio
---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+        My name is Antonio Marino, Welcome to my portfolio I am a 24 year old aspiring web developer based in Columbus Ohio. I am working throught the 12 week Web Dev program at The Ohio State University.           
+        </p>
+        <p>
+        Past expierence: I worked at Scotts Miracle Gro for 2 years with the Digital Expierence team. My role within this team was Digital content manager, I would use a CMS to update, create, and maintain our content or products! 
+        This expierence was a very important role in my decision to become a Developer! 
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;

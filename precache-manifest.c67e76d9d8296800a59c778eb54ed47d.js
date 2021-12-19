26c87c728eaef5bff92750032a2c3023903b13e2
Antonio Marino <marino.antonio375@gmail.com>
2021-12-19 13:09:11 -0500

    Updates

---
+++ precache-manifest.c67e76d9d8296800a59c778eb54ed47d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5a8ba25d76f68fb43a7823b893aa8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165ecf23c26b5f26df11",
+    "url": "/static/css/main.ff9e07b9.chunk.css"
+  },
+  {
+    "revision": "6947b6a6743aea9bb900",
+    "url": "/static/js/2.b9788bfc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b9788bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165ecf23c26b5f26df11",
+    "url": "/static/js/main.0373e0d5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "62128d47247cae5885b99dda00ba1088",
+    "url": "/static/media/book-search.62128d47.jpg"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "9f2caf0d90432d8480062b3f9fee0bc0",
+    "url": "/static/media/fitness-tracker.9f2caf0d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "70c7d1604d5eb26ed524cda8a273cd70",
+    "url": "/static/media/project-3-nfl.70c7d160.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/static/media/surf-report.12557c9b.jpg"
+  }
+]);

b4948a53c4b82ff17db24bfdf10a1a9993c7f300
Antonio Marino <marino.antonio375@gmail.com>
2021-12-20 17:29:12 -0500

    Updates

---
+++ precache-manifest.692ea940fd36dcd19341ac843ad85ebc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81a84b6c8c1b8e880a5e4d9284c6daa",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "0d323d975c851e70b399",
+    "url": "/React_Portfolio/static/css/main.ff9e07b9.chunk.css"
+  },
+  {
+    "revision": "6947b6a6743aea9bb900",
+    "url": "/React_Portfolio/static/js/2.b9788bfc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React_Portfolio/static/js/2.b9788bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d323d975c851e70b399",
+    "url": "/React_Portfolio/static/js/main.3e5e8262.chunk.js"
+  },
+  {
+    "revision": "3a0598dfa4517aaf1428",
+    "url": "/React_Portfolio/static/js/runtime-main.446e9683.js"
+  },
+  {
+    "revision": "62128d47247cae5885b99dda00ba1088",
+    "url": "/React_Portfolio/static/media/book-search.62128d47.jpg"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/React_Portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/React_Portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "9f2caf0d90432d8480062b3f9fee0bc0",
+    "url": "/React_Portfolio/static/media/fitness-tracker.9f2caf0d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/React_Portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "70c7d1604d5eb26ed524cda8a273cd70",
+    "url": "/React_Portfolio/static/media/project-3-nfl.70c7d160.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/React_Portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/React_Portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c67e76d9d8296800a59c778eb54ed47d.js"
+  "/React_Portfolio/precache-manifest.692ea940fd36dcd19341ac843ad85ebc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
